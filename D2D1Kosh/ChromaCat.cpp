24ce11368844a1c3029e121b0d16a598b0e7959a
Olga Pshenichnikova <ya.olgapshenichnikova@yandex.ru>
2020-03-13 13:53:20 +0200

    Code reorganizing;

---
+++ D2D1Kosh/ChromaCat.cpp
@@ -0,0 +1,1 @@
+#include "ChromaCat.h"
---
+++ D2D1Kosh/common.h
@@ -0,0 +1,47 @@
+#pragma once
+
+// Windows Header Files:
+#include <windows.h>
+
+// C RunTime Header Files:
+#include <stdlib.h>
+#include <malloc.h>
+#include <memory.h>
+#include <wchar.h>
+#include <math.h>
+
+#include <d2d1.h>
+#include <d2d1helper.h>
+#include <dwrite.h>
+#include <wincodec.h>
+
+#pragma comment(lib, "d2d1.lib")
+
+#ifndef Assert
+#if defined( DEBUG ) || defined( _DEBUG )
+#define Assert(b) do {if (!(b)) {OutputDebugStringA("Assert: " #b "\n");}} while(0)
+#else
+#define Assert(b)
+#endif //DEBUG || _DEBUG
+#endif
+
+#ifndef HINST_THISCOMPONENT
+EXTERN_C IMAGE_DOS_HEADER __ImageBase;
+#define HINST_THISCOMPONENT ((HINSTANCE)&__ImageBase)
+#endif
+
+#define RET_HR(_f) hr = _f; if (FAILED(hr)) return hr;
+#define RET_HR_NULL( _f, _obj ) hr = _f; if (FAILED(hr) || !_obj) return FAILED(hr) ? hr : S_FALSE;
+
+template<class Interface>
+inline void SafeRelease(
+    Interface** ppInterfaceToRelease
+)
+{
+    if (*ppInterfaceToRelease != NULL)
+    {
+        (*ppInterfaceToRelease)->Release();
+
+        (*ppInterfaceToRelease) = NULL;
+    }
+}
